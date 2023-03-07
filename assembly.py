e7d6f346116c42d24b78259ae6b29091065f7249
singinglia <48102615+singinglia@users.noreply.github.com>
2023-03-07 14:23:56 -0800

    Updated with discussion points

--- assembly.py
+++ assembly.py
@@ -1,7 +1,22 @@
-def scorePatternList():
-    return len()
+from DouglasFunctions import *
 
-def AvengersAssemble(I, E, Match, lmin, t=100):
+def scorePatternList(patternList):
+    return len(patternList)
+
+def adjustIndexes(currIdxes, augment = None, shift = None):
+    newIdxs = []
+    if augment is not None:
+        for idx in currIdxes:
+            newIdxs.append((idx[0]+1, idx[1]))
+
+    elif shift is not None:
+        for i, idx in enumerate(currIdxes):
+            newIdxs.append((shift[1], idx[1]))
+
+    return newIdxs
+
+def AvengersAssemble(I, E, Match, lmin, jump=1, t=100):
+    FAIL_MAX = 100
     I_lens = [len(x) for x in I]
 
     P = None
@@ -13,26 +28,33 @@ def AvengersAssemble(I, E, Match, lmin, t=100):
 
         currPatternList = []
         # While something
-        while something:
-            segments = chop(I, currIndices, lmin)
-            # Search Segments for Patterns
-            for seg in segments:
-                # The order of things here will depend on Bhavya's implementation
-                possPattern = Search(I, seg, Match, lmin)
+        failCount = 0
+        while True:
 
-                if len(possPattern) == 0:
-                    #Do something
+            try:
+                segments = chop(I, currIndices, lmin)
+            except:
+                break
+
+            # The order of things here will depend on Bhavya's implementation
+            possPattern = Search(I, segments, Match, lmin)
+
+            if len(possPattern) == 0:
+                failCount += 1
+                if failCount >= FAIL_MAX:
+                    break
+                else:
+                    currIndices = adjustIndexes(currIndices, augment=jump)
                     continue
 
-                if possPattern is not None:
-                    ppMedian = median_string(I, possPattern)
-                    if is_excluded(ppMedian, ex_dic=exDict):
-                        continue
-                    else:
-                        currPatternList.append(ppMedian)
-                        #Possibly add to a library here as well?
-
-                        #TODO Build New currIndex List
+            if possPattern is not None:
+                ppMedian = median_string(I, possPattern)
+                if is_excluded(ppMedian, ex_dic=exDict):
+                    continue
+                else:
+                    # Possibly add to a library here as well?
+                    currPatternList.append(ppMedian)
+                    currIndices = adjustIndexes(currIndices, shift=possPattern)
 
         # Score Pattern List, if better than Max than new best
         if scorePatternList(currPatternList) > scorePatternList(P):
