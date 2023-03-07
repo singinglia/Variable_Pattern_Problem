97806b4b1cc42f01295763782e5238b047604918
singinglia <48102615+singinglia@users.noreply.github.com>
2023-03-07 13:07:35 -0800

    Added Score function

--- assembly.py
+++ assembly.py
@@ -1,11 +1,15 @@
+def scorePatternList():
+    return len()
+
 def AvengersAssemble(I, E, Match, lmin, t=100):
     I_lens = [len(x) for x in I]
 
     P = None
 
+    currIndices = I_lens
+    exDict = get_excluded_dic(E, lmin)
+
     for i in range(t):
-        currIndices = I_lens
-        exDict = get_excluded_dic(E, lmin)
 
         currPatternList = []
         # While something
@@ -15,6 +19,11 @@ def AvengersAssemble(I, E, Match, lmin, t=100):
             for seg in segments:
                 # The order of things here will depend on Bhavya's implementation
                 possPattern = Search(I, seg, Match, lmin)
+
+                if len(possPattern) == 0:
+                    #Do something
+                    continue
+
                 if possPattern is not None:
                     ppMedian = median_string(I, possPattern)
                     if is_excluded(ppMedian, ex_dic=exDict):
@@ -23,8 +32,10 @@ def AvengersAssemble(I, E, Match, lmin, t=100):
                         currPatternList.append(ppMedian)
                         #Possibly add to a library here as well?
 
+                        #TODO Build New currIndex List
+
         # Score Pattern List, if better than Max than new best
-        if len(currPatternList) > len(P):
+        if scorePatternList(currPatternList) > scorePatternList(P):
             P = currPatternList
 
         # Alternatively, Store patterns to a library to be combined for the best possible pattern list
